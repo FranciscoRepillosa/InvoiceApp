185bd7a1e7acfbb830bda3029ec567f5bff4effd
Francisco F R. Repillosa <frepisolla@redcoincoin.lan>
2023-03-29 13:40:22 -0400

    db connection options

--- test.js
+++ test.js
@@ -1,22 +1,16 @@
-// This is a simple Node.js server that listens on port 3000 and responds with a message when a GET request is made to the root path.
-// Express is a Node.js web application framework that provides a set of features for web and mobile applications.
-// The app.listen() method instructs the server to begin listening for client requests on the specified port and host.
-// The app.get() method specifies a callback function to be executed when a client requests a particular route (in this case, the root path).
-// The callback function takes two arguments: the request and response objects.
-// The response object contains a send() method that can be used to send a response to the client.
-
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = 6666
 
-mongoose.connect("mongodb+srv://frenchi:O197YM4p8Q3d0HS2@db-mongodb-nyc1-42599-07ea1556.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-42599", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    }
+const conectionString = "mongodb+srv://frenchi:O197YM4p8Q3d0HS2@db-mongodb-nyc1-42599-07ea1556.mongo.ondigitalocean.com/invoice?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-42599"
+
+mongoose.connect(conectionString, {
+  serverSelectionTimeoutMS: 5000  
+}
 );
+
+
 
 // require the routes file of the invoice folder
 const invoiceRoutes = require('./invoice/routes.config.js');
