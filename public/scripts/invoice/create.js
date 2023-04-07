b4eb10b7ee1d8bef26e8c9ac50524c4ad9201583
Francisco F R. Repillosa <frepisolla@redcoincoin.lan>
2023-04-07 13:36:32 -0400

    client side scripts for create invoice

---
+++ public/scripts/invoice/create.js
@@ -0,0 +1,24 @@
+document.getElementById('Submit').addEventListener('click', function() {
+    // get the total and email from the form and send it to the server using fetch API
+    // to invoice route
+    var total = document.getElementById('total').value;
+    var email = document.getElementById('email').value;
+    var data = {
+        total: total,
+        email: email
+    };
+    fetch('/invoice', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        if (response.status === 200) {
+            window.location.href = '/invoice/list';
+        } else {
+            alert('Invalid username or password');
+        }
+    });
+}
+)
