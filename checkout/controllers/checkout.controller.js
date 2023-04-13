91b1ffe8cc8dc36477fe1f1a2e25b274223f9d14
Francisco F R. Repillosa <frepisolla@redcoincoin.lan>
2023-04-13 10:55:22 -0400

    adding checkout module

--- checkout/controllers/checkout.controller.js
+++ checkout/controllers/checkout.controller.js
@@ -1,5 +1,5 @@
 const stripe = require("stripe")('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-const Invoice = require('../../invoice/models/invoice.model.js');
+const Invoice = require('../../invoice/model/invoice.model.js');
 
 const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
